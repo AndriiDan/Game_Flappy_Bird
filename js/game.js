e16dab89cb24e651c943a9a5599cbd3f8b671248
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-03-27 10:27:19 +0300

    Add object images.

---
+++ js/game.js
@@ -0,0 +1,38 @@
+// об'єкт з html
+var canvas = document.getElementById("canvas");
+
+// вказую 2d - вид гри
+var context = canvas.getContext("2d");
+
+// створення зображень
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+// завантаження зображень
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
+
+// відстань по висоті між трубами
+var gap = 90; // 90px
+
+// малюю зображення в canvas
+function draw() {
+    // в canvas зображення bg(background) з координатами х:0, у:0
+    context.drawImage(bg, 0, 0);
+
+    context.drawImage(pipeUp, 100, 0); // труба зверху
+    context.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap); // розміщення відносно висоти pipeUp і змінної "gap" (відступ між блоками)
+
+    context.drawImage(fg, 0, canvas.height - fg.height); // по "у": опустити fg(forground) вниз
+
+    context.drawImage(bird, 10, 200);
+}
+
+// Необхідно викликати ф-цію draw() тільки після завантаження всіх картинок. Тому, коли завантахеться остання картинка (в моєму випадку pipeBottom) - викликаю ф-цію draw()
+pipeBottom.onload = draw;
