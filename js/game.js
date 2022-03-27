d75677b29027358e70ee90c0400f4df8341c8a4a
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-03-27 19:43:40 +0300

    Зміна позиції пташки.

--- js/game.js
+++ js/game.js
@@ -21,6 +21,19 @@ pipeBottom.src = "img/pipeBottom.png";
 // відстань по висоті між трубами
 var gap = 90; // 90px
 
+// при натисканні на будь-яку клавішу, буде викликатися ф-ція moveUp, яка підіймає пташку по осі у на 20 вверх
+document.addEventListener("keydown", moveUp);
+
+// підіймає пташку по осі у на 20 вверх
+function moveUp() {
+    yPos -= 20;
+}
+
+// позиція пташки
+var xPos = 10;
+var yPos = 200;
+var grav = 1; // позиція пташки по осі у буде змінюватися на 1
+
 // малюю зображення в canvas
 function draw() {
     // в canvas зображення bg(background) з координатами х:0, у:0
@@ -31,7 +44,11 @@ function draw() {
 
     context.drawImage(fg, 0, canvas.height - fg.height); // по "у": опустити fg(forground) вниз
 
-    context.drawImage(bird, 10, 200);
+    context.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    // постійний виклик ф-ції (метода)
+    requestAnimationFrame(draw);
 }
 
 // Необхідно викликати ф-цію draw() тільки після завантаження всіх картинок. Тому, коли завантахеться остання картинка (в моєму випадку pipeBottom) - викликаю ф-цію draw()
